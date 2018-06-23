625caa8531e8a48a0ae67bcfa339aa44257be83d
hzj5158888 <www1693303798@163.com>
2018-06-23 18:23:36 +0800

    fix

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2213,7 +2213,7 @@ static void get_scan_count(struct lruvec *lruvec, int swappiness,
 	 * system is under heavy pressure.
 	 */
 	if (!IS_ENABLED(CONFIG_BALANCE_ANON_FILE_RECLAIM) &&
-	    !inactive_file_is_low(lruvec)) {
+	    !inactive_file_is_low(lruvec) &&
 	    get_lru_size(lruvec, LRU_INACTIVE_FILE) >> sc->priority) {
 		scan_balance = SCAN_FILE;
 		goto out;
