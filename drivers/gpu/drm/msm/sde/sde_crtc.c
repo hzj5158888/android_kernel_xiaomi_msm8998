591e63f9884f06223887740f41da8a80247328d3
Francisco Franco <franciscofranco.1990@gmail.com>
2018-08-12 16:06:21 +0000

    drm: msm: sde: Boost cpubw devfreq device when a new frame is ready
    The cpubw devfreq device delegates the speed of the DDR bus, so its
    speed is directly correlated with frame drops. Boost it when a new frame
    is ready to be rendered to the display to reduce overall frame drops.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -20,6 +20,11 @@
 #include <linux/sort.h>
 #include <linux/debugfs.h>
 #include <linux/ktime.h>
+<<<<<<< HEAD
+=======
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+>>>>>>> c9d0d51e70fe... drm: msm: sde: Boost cpubw devfreq device when a new frame is ready
 #include <uapi/drm/sde_drm.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_crtc.h>
@@ -950,6 +955,13 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc)
 
 
 	SDE_ATRACE_BEGIN("crtc_commit");
+<<<<<<< HEAD
+=======
+
+	cpu_input_boost_kick();
+	devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+
+>>>>>>> c9d0d51e70fe... drm: msm: sde: Boost cpubw devfreq device when a new frame is ready
 	list_for_each_entry(encoder, &dev->mode_config.encoder_list, head) {
 		if (encoder->crtc != crtc)
 			continue;
