a04c026a50e27fc4071013f01bca44383840948f
hzj5158888 <www1693303798@163.com>
2018-11-04 11:41:56 +0800

    Revert "qpnp: fg+smb-lib: implement Linux cycle count"
    This reverts commit ed9ac7c6d75ab12bd5c66ccf842459a4c6c16359.

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1034,9 +1034,7 @@ static enum power_supply_property smb2_batt_props[] = {
 	POWER_SUPPLY_PROP_CHARGE_COUNTER,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
 	POWER_SUPPLY_PROP_CHARGE_FULL,
-#ifndef CONFIG_QPNP_FG_GEN3_LEGACY_CYCLE_COUNT
 	POWER_SUPPLY_PROP_CYCLE_COUNT,
-#endif
 };
 
 static int smb2_batt_get_prop(struct power_supply *psy,
@@ -1144,11 +1142,6 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		val->intval = chg->fcc_stepper_mode;
 		break;
-#ifndef CONFIG_QPNP_FG_GEN3_LEGACY_CYCLE_COUNT
-	case POWER_SUPPLY_PROP_CYCLE_COUNT:
-		rc = smblib_get_cycle_count(chg, val);
-		break;
-#endif
 	default:
 		pr_err("batt power supply prop %d not supported\n", psp);
 		return -EINVAL;
